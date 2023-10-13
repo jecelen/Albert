33b8e75f9afe677dde47f3427717105899ce1c73
jecelen <112420534+jecelen@users.noreply.github.com>
2023-10-13 03:03:03 -0400

    atualizacao de login com dados de user ficticio

--- pages/login/script.js
+++ pages/login/script.js
@@ -1,3 +1,22 @@
 function voltar(){
-    window.location.href = "../inicial/index.html"
+    window.location.href = "../inicial/index.html";
+}
+
+function verificar(){
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+    fetch('../../assets/userExemple.json')
+        .then(Response => Response.json())
+        .then(data => {
+            if(email == data.email && senha == data.senha){
+                window.location.href = "../inicial/index.html";
+            }
+            else{
+                alert("E-mail ou senha inválido(a)!");
+            }
+            })
+        .catch(error => {
+            console.error("Erro ao carregar o arquivo JSON:", error);
+        });
+
 }
