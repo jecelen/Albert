22c1e7e2c00369e66e22e08cfc3961c774b2c64e
Vanessa <130495689+eissenv@users.noreply.github.com>
2023-10-13 23:12:34 -0400

    Telas de ADM

---
+++ adm/script.js
@@ -0,0 +1,11 @@
+function paginaCriar() {
+    window.location.href = "./paginas/criar/index.html";
+}
+
+function paginaEditar() {
+    window.location.href = "./paginas/editar/index.html";
+}
+
+function paginaDeletar() {
+    window.location.href = "./paginas/deletar/index.html";
+}
