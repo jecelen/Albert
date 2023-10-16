a6d7a4afca2d369af0fd3c2d7d4e8c86de65edd0
Vanessa <130495689+eissenv@users.noreply.github.com>
2023-10-15 22:41:00 -0400

    Arrumando conexão entre páginas

---
+++ adm/paginas/deletarTema/script.js
@@ -0,0 +1,27 @@
+function finalizarPag() {
+  window.location.href = '../../../adm/index.html';
+}
+
+function sair() {
+  window.location.href = "../../../pages/inicial/index.html";
+}
+
+function meuPerfil() {
+  window.location.href = "../../../pages/perfilUsuario/index.html";
+}
+
+function temaBiologia() {
+  window.location.href = "../../../adm/paginas/deletar/index.html";
+}
+
+function temaGeografia() {
+  window.location.href = "../../../adm/paginas/deletar/index.html";
+}
+
+function temaHistoria() {
+  window.location.href = "../../../adm/paginas/deletar/index.html";
+}
+
+function temaEsportes() {
+  window.location.href = "../../../adm/paginas/deletar/index.html";
+}
