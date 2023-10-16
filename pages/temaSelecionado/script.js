331ed3878d3c54742b30ccb3c590a4b1cfea5266
Guilherme Cupertino <114947249+GuilhermeCupertino@users.noreply.github.com>
2023-10-15 23:42:52 -0400

    Update script.js

--- pages/temaSelecionado/script.js
+++ pages/temaSelecionado/script.js
@@ -1,4 +1,14 @@
 function sair() {
     window.location.href = "../inicial/index.html";
   }
+
+function desistencia(){
+      alert("Você desistiu do jogo");
+      window.location.href = "../bemVindo/index.html";  
+  }
+
+  function desistencia(){
+    alert("Ainda não conseguimos conferir se a sua resposta está certa, por favor aguarde!");
+    window.location.href = "../temaSelecionado/index.html";  
+}
   
