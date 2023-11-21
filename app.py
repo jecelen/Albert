1d449e8259c0302beeeb8c8c88dc710f1ce914e7
jecelen <112420534+jecelen@users.noreply.github.com>
2023-11-21 14:28:06 -0400

    Adicionei a chamada automática do localhost

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from routes.routes import routes_blueprint
+import webbrowser
 
 app = Flask(__name__)
 
 app.register_blueprint(routes_blueprint)
 
 if __name__ == '__main__':
+    webbrowser.open_new('http://127.0.0.1:5000/')
     app.run(debug=True)
