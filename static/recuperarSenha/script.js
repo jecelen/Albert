59e9cd15e3d8d8c3eeb9eab463822d164760cc54
jecelen <112420534+jecelen@users.noreply.github.com>
2023-11-21 14:35:54 -0400

    Alteração rota recuperar senha

--- static/recuperarSenha/script.js
+++ static/recuperarSenha/script.js
@@ -2,3 +2,7 @@ function confirmacao() {
   alert("Você receberá um e-mail com sua nova senha de acesso!");
   window.location.href = "../";
 }
+
+function voltar() {
+  window.location.href = "../";
+}
