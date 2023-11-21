c78d62c21203e8225f4f1a64fcfe85a2be98abe6
jecelen <112420534+jecelen@users.noreply.github.com>
2023-11-21 12:30:18 -0400

    Delete pages directory -- dentro de templates agora

--- pages/inicial/script.js
+++
@@ -1,9 +0,0 @@
-function registro() {
-  window.location.href = "../registro/index.html";
-}
-
-function entrar() {
-  window.location.href = "../login/index.html";
-}
-
-
--- pages/registro/script.js
+++
@@ -1,9 +0,0 @@
-function voltar() {
-  window.location.href = "../inicial/index.html";
-}
-
-function mensagem() {
-  alert(
-    "Estamos sem banco de dados por enquanto :( Tente se registrar novamente na próxima etapa do trabalho!"
-  );
-}
