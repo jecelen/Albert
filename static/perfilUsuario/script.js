b76de41ad191a112839d1b0ef1fca67c841c9a96
Guilherme Cupertino <114947249+GuilhermeCupertino@users.noreply.github.com>
2023-11-25 18:39:15 -0400

    Update script.js

--- static/perfilUsuario/script.js
+++ static/perfilUsuario/script.js
@@ -5,23 +5,79 @@ function voltar() {
 function meuPerfil() {
   window.location.href = "../perfil";
 }
+/*
 function user() {
   var nome;
   var mail;
-  fetch("../../../assets/userExemple.json")
+  fetch("../../../static/assets/userExemple.json")
     .then((Response) => Response.json())
     .then((data) => {
       nome = data.unome;
       mail = data.uemail;
+      console.log(nome);
       document.getElementById("nome").innerHTML = ": " + nome;
       document.getElementById("mail").innerHTML = ": " + mail;
-      "Seja bem vindo(a), " + nome + ".";
+      "Seja bem vindo(a), " + nome + "."; 
+      
     })
+    
+    .catch((error) => {
+      console.error("Erro ao carregar o arquivo JSON:", error);
+    });
+}
+*/
+
+function userNome() {
+  var nome;
+  fetch("../../../static/assets/userExemple.json")
+    .then((Response) => Response.json())
+    .then((data) => {
+      nome = data.unome;
+      document.getElementById("username").value = nome; 
+      
+    })
+    
     .catch((error) => {
       console.error("Erro ao carregar o arquivo JSON:", error);
     });
 }
 
+function userMail() {
+  var mail;
+  fetch("../../../static/assets/userExemple.json")
+    .then((Response) => Response.json())
+    .then((data) => {
+      mail = data.uemail;
+      document.getElementById("email").value = mail; 
+      
+    })
+    
+    .catch((error) => {
+      console.error("Erro ao carregar o arquivo JSON:", error);
+    });
+}
+
+function userSenha() {
+  var senha;
+  fetch("../../../static/assets/userExemple.json")
+    .then((Response) => Response.json())
+    .then((data) => {
+      senha = data.usenha;
+      document.getElementById("senha").value = senha; 
+      
+    })
+    
+    .catch((error) => {
+      console.error("Erro ao carregar o arquivo JSON:", error);
+    });
+}
+
+
+
+
+
+
+/*
 let usuario = {
   username: "Shawlin",
   email: "user@mail.com",
@@ -53,3 +109,4 @@ function updateUser() {
 
 // Preencher o formulário com as informações do usuário ao carregar a página
 preencherFormulario();
+*/
